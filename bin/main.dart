816536c89e9ff80233e04e61fcfb93c70255b70f
liuxu <18270719298@163.com>
2019-03-11 11:04:41 +0800

    加入const和final的实验工作

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,6 @@
 import 'package:megatron/megatron.dart' as megatron;
 import 'package:megatron/action_type.dart' as action_type;
+import 'package:megatron/variables.dart' as variables;
 
 main(List<String> arguments) {
   print('Hello world: ${megatron.calculate()}!');
@@ -7,4 +8,9 @@ main(List<String> arguments) {
   String typeName = action_type.typeName();
 
   print('获取到的类型内容:${typeName}');
+
+  variables.showApplicationName();
+  variables.showApplicationKey();
+  variables.showNow();
+  variables.assertVariable();
 }
--- lib/action_type.dart
+++ lib/action_type.dart
@@ -3,6 +3,10 @@
 /// 返回类型
 /// 其他返回类型也可以省略返回类型，不过按照好习惯，我觉得还是要写上返回类型
 /// 我猜测啊，这个void可能在Dart看来就是一种Object，毕竟Dart是全面向对象的语言
+/// Everything you can place in a variable is an object, and every
+/// object is an instance of a class.
+/// Even numbers, functions, and null are objects. All objects inherit from the Object class.
+/// 上面这句话翻译：所有的都是对象，都是继承自Object类
 
 void printLog(){
   print('打印一个具体信息类型的日志!');
