39eb8046ea527b5a21880396f12d09b470daaa42
lxzxx <928525166@qq.com>
2019-03-17 21:55:51 +0800

    Add

--- bin/main.dart
+++ bin/main.dart
@@ -2,7 +2,7 @@ import 'package:megatron/megatron.dart' as megatron;
 import 'package:megatron/action_type.dart' as action_type;
 import 'package:megatron/variables.dart' as variables;
 
-main(List<String> arguments) {
+void main(List<String> arguments) {
   print('Hello world: ${megatron.calculate()}!');
   action_type.printLog();
   String typeName = action_type.typeName();
